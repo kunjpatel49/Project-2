71d53ee4367890661aa73d07186e08cb2f08c7be
Kunj Patel <kpate331@uic.edu>
2019-10-30 00:17:26 +0000

    debugging

--- sw.js
+++ sw.js
@@ -6,25 +6,25 @@ var appShellFiles = [
 
 //install event
 self.addEventListener('install', evt=> {
-  //console.log('service worker installed');
-  evt.waitUntil(caches.open(staticCache).then(cache=>{
-        console.log('cache adding');
-        cache.addAll(assets);                        
-      })
-    );
+  //console.log('service worker installed');
+  evt.waitUntil(caches.open(staticCache).then(cache=>{
+        console.log('cache adding');
+        cache.addAll(assets);                        
+      })
+    );
 });
 
 //activate event
 self.addEventListener('activate', evt=>{
-      //console.log('service worker activated');
+      //console.log('service worker activated');
 });
 
 //fetch event
 self.addEventListener('fetch', evt=>{
-          //console.log('fetch event', evt);
-         evt.respondWith(
-             caches.match(evt.request).then(cacheRes =>{
-                 return cacheRes || fetch(evt.request);
-             })
-         );
+          //console.log('fetch event', evt);
+         evt.respondWith(
+             caches.match(evt.request).then(cacheRes =>{
+                 return cacheRes || fetch(evt.request);
+             })
+         );
 });
